9e5a7ecbdfbca12bad2957d4021ff963f240610f
Adolfo Gómez <dkmaster@dkmon.com>
2014-08-20 22:33:09 +0200

    added keyboard control to tile tests

--- tile/test.py
+++ tile/test.py
@@ -25,7 +25,10 @@ m.drawTo(screen, 0, 100)
 run = True
 full = False
 t_ref = 0
-x = 0
+x = y = 0
+x_speed = 0
+y_speed = 0
+
 while run:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -33,11 +36,25 @@ while run:
         if event.type == KEYDOWN:
             if event.key == 113:
                 run = False
-        if event.type == KEYUP:
-            pass
+            elif event.key == K_RIGHT:
+                x_speed = 1
+            elif event.key == K_LEFT:
+                x_speed = -1
+            elif event.key == K_UP:
+                y_speed = -1
+            elif event.key == K_DOWN:
+                y_speed = 1
 
-    m.drawTo(screen, x, 100)
-    x += 1
+
+        if event.type == KEYUP:
+            if event.key in (K_RIGHT, K_LEFT):
+                x_speed = 0
+            elif event.key in (K_UP, K_DOWN):
+                y_speed = 0
+
+    m.drawTo(screen, x, y)
+    x += x_speed
+    y += y_speed
 
     t = pygame.time.get_ticks()
     #pygame.time.wait(60-t+t_ref)
