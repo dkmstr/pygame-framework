a168743f4260132e96f226de3afdf5f2da0d4f5c
Adolfo Gómez <dkmaster@dkmon.com>
2014-09-02 03:40:06 +0200

    start big refactoring

--- resources/resources/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import util
-import maps
-import images
-import game_state
--- resources/resources/maps/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-import logging
-from resources.maps.maps import Maps
-
-logger = logging.getLogger(__name__)
-
