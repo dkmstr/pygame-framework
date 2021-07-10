1ceea8df2f006ad627831c1619a14818a589d093
Adolfo Gómez García <dkmaster@dkmon.com>
2021-07-10 22:07:54 +0200

    Addind typechecking to nuvelin

--- src/nivelin.py
+++ src/nivelin.py
@@ -4,18 +4,19 @@
 import logging
 import tempfile
 import os
+import typing
 
 # Early logger configuration, before imports so we can use logger everywhere
 logging.basicConfig(
     filename=os.path.join(tempfile.gettempdir(), 'log.log'),
     filemode='w',
     format='%(levelname)s %(asctime)s %(module)s %(funcName)s %(lineno)d %(message)s',
-    level=logging.DEBUG
+    level=logging.DEBUG,
 )
 
 # Main imports
 import pygame
-from pygame.locals import *
+from pygame import constants as const
 from player import Player
 from items import *
 import game
@@ -34,6 +35,8 @@ BASE_SPEED = 8
 
 
 class GameTest(game.game_state.GameState):
+    store: SoundsStore
+    maps: typing.Optional[game.maps.Maps]
 
     def __init__(self, name):
         super(GameTest, self).__init__(name)
@@ -41,37 +44,53 @@ class GameTest(game.game_state.GameState):
         self.bg_speed = 0
 
         self.maps = None  # Initialized on "on_init"
+        self.store = SoundsStore.store  # type: ignore
 
         self.map = None
-        self.pressKey = { K_RIGHT: Player.goRight, K_LEFT: Player.goLeft, K_DOWN: Player.goDown, K_UP: Player.goUp, K_SPACE: Player.jump }
-        self.releaseKey = { K_RIGHT: Player.stopRight, K_LEFT: Player.stopLeft, K_DOWN: Player.stopDown, K_UP: Player.stopUp, K_SPACE: Player.stopJump }
+        self.pressKey = {
+            const.K_RIGHT: Player.goRight,
+            const.K_LEFT: Player.goLeft,
+            const.K_DOWN: Player.goDown,
+            const.K_UP: Player.goUp,
+            const.K_SPACE: Player.jump,
+        }
+        self.releaseKey = {
+            const.K_RIGHT: Player.stopRight,
+            const.K_LEFT: Player.stopLeft,
+            const.K_DOWN: Player.stopDown,
+            const.K_UP: Player.stopUp,
+            const.K_SPACE: Player.stopJump,
+        }
 
     def on_init(self):
         self.maps = game.maps.Maps(self.controller)
-        #self.maps.add('level0', 'data/maps/kenney/level-test-1.tmx')
+        # self.maps.add('level0', 'data/maps/kenney/level-test-1.tmx')
         self.maps.add('level0', 'data/maps/kenney/level-test-1.tmx')
         # Register actors types
         actorsFactory.registerType('Player', Player)
-        #SoundsStore.store.storeMusicFile('level0', 'journey_3.ogg', volume=0.5)
-        SoundsStore.store.storeMusicFile('level0', 'Rose Flats.ogg', volume=0.1)
+        # SoundsStore.store.storeMusicFile('level0', 'journey_3.ogg', volume=0.5)
+        self.store.storeMusicFile('level0', 'Rose Flats.ogg', volume=0.1)  
 
-        #self.images.addImage('bck1', 'data/images/far-background.png')
-        #self.images.addImage('bck2', 'data/images/near-background.png')
-        #self.images.load(self.controller.screen)
+        # self.images.addImage('bck1', 'data/images/far-background.png')
+        # self.images.addImage('bck2', 'data/images/near-background.png')
+        # self.images.load(self.controller.screen)
 
-        #self.maps.add('level0', 'data/maps/other.tmx')
+        # self.maps.add('level0', 'data/maps/other.tmx')
         self.maps.load()
 
         self.map = self.maps.get('level0')
         self.player = list(self.map.getActors('Player'))[0]
-        self.map.addHudElement(ScoreFilesHud(self.player, 'data/images/numbers/hud_*.png', 8, 5, 5))
+        self.map.addHudElement(
+            ScoreFilesHud(self.player, 'data/images/numbers/hud_*.png', 8, 5, 5)
+        )
 
-        #self.bg.add_surface(self.images.get('bck1'), 5)
-        #self.bg.add_surface(self.images.get('bck2'), 3)
+        # self.bg.add_surface(self.images.get('bck1'), 5)
+        # self.bg.add_surface(self.images.get('bck2'), 3)
 
     def on_enter(self):
-        SoundsStore.store.get('level0').play()
-        pass
+        level0 = self.store.get('level0')
+        if level0:
+            level0.play()
 
     def on_exit(self):
         pygame.mixer.music.stop()
@@ -84,10 +103,10 @@ class GameTest(game.game_state.GameState):
         return None
 
     def on_keyup(self, key):
-        if key == K_q:
+        if key == const.K_q:
             return game.game_state.GameControl.EXIT_GAMESTATE
 
-        #if key == K_n:
+        # if key == K_n:
         #    return 'state1'
 
         fnc = self.releaseKey.get(key)
@@ -97,31 +116,39 @@ class GameTest(game.game_state.GameState):
         return None
 
     def on_frame(self):
-        #self.bg.scroll(self.bg_speed)
+        # self.bg.scroll(self.bg_speed)
         self.map.update()
-        #self.player.move(self.x_speed, self.y_speed)
+        # self.player.move(self.x_speed, self.y_speed)
 
         self.player.updateMapDisplayPosition(self.controller.renderer)
         return None
 
     def on_render(self):
-        #self.bg.draw(self.controller.screen)
+        # self.bg.draw(self.controller.screen)
         self.map.draw(self.controller.renderer)
-        #self.player.draw(self.controller.screen)
+        # self.player.draw(self.controller.screen)
         return None
+
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = 'center'
 
 # Set defaults sound settings before initializing pygame
-pygame.mixer.pre_init(44100,-16,2, 1024)
+pygame.mixer.pre_init(44100, -16, 2, 1024)
 
-gc = game.game_state.GameControl(WIDTH, HEIGHT, framerate=50, enableFrameSkip=True, fullScreen=FULLSCREEN, renderer=RENDERER)
+gc = game.game_state.GameControl(
+    WIDTH,
+    HEIGHT,
+    framerate=50,
+    enableFrameSkip=True,
+    fullScreen=FULLSCREEN,
+    renderer=RENDERER,
+)
 
 gc.add(GameTest('state0'))
 
 import cProfile
 
 cProfile.run('gc.run()', os.path.join(tempfile.gettempdir(), 'test.stats'))
-#gc.run()
+# gc.run()
 
 gc.quit()
